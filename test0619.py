b8677ef36e945b55e57a8948716a5e60d19a7daa
LJWclown <1617141444@qq.com>
2019-06-19 21:39:05 +0800

    test0619

---
+++ test0619.py
@@ -0,0 +1,30 @@
+#1、反弹球
+# h=int(input('请输入初始高度（米）'))
+# n=h
+# for i in range(1,11):
+#     h/=2
+#     n+=h*2
+#     if i==10:
+#         print(n-h*2)
+#         print(h)
+
+
+#2、计算人口多少年到达26亿
+# population=13
+# count=0
+# while True:
+#     if population<26:
+#         population*=1.008
+#         count+=1
+#     else:
+#         print(count)
+#         break
+
+
+#求1！+2！+3！+4！+5！的和
+# n=1
+# sum=0
+# for i in range(1,6):
+#     n=i*n
+#     sum=sum+n
+# print(sum)
